a31a75a698c0eefa7d1c09c5cd9b19bab0275f9d
Markus Stahl <markus.stahl@humanitec.com>
2023-07-24 12:23:52 +0200

    how to make this action visible

--- src/action.ts
+++ src/action.ts
@@ -1,5 +1,5 @@
 import {getOctokit, context} from '@actions/github';
-import {getInput, setOutput, info, exportVariable} from '@actions/core';
+import {getInput, setOutput, warning, exportVariable} from '@actions/core';
 import {render} from 'mustache';
 
 import {branchNameToEnvId} from './utils';
@@ -237,7 +237,7 @@ export async function runAction(): Promise<void> {
     environmentUrl = render(environmentUrlTemplate, templateParams);
   }
   // TODO: setOutput('environment-url', environmentUrl);
-  info('Use environment-url: https://dev-02-ci-test-on-pr-app.humanitec.io/');
+  warning('Use environment-url: https://dev-02-ci-test-on-pr-app.humanitec.io/');
   setOutput('environment-url', 'https://dev-02-ci-test-on-pr-app.humanitec.io/');
   exportVriable('HUMANITEC_PREVIEW_URL', 'https://dev-02-ci-test-on-pr-app.humanitec.io/');
 
